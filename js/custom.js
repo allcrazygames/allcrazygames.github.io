0d5530f8d447176a83794ff36ded5c3ec85b12fc
allcrazygames <brettj.fuentes01@gmail.com>
2025-02-10 18:30:31 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,11 +6,27 @@ This file will not be overwritten by the updater
 
 */
 
+// Google Analytics code
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4JHYWX69KB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4JHYWX69KB');
+</script>
+
 // JavaScript code
 function search_animal() {
   let input = document.getElementById("searchbar").value;
   input = input.toLowerCase();
   let x = document.getElementsByClassName("animals");
+
+  // Trigger Google Analytics event for the search
+  gtag('event', 'search', {
+    'search_term': input
+  });
 
   for (i = 0; i < x.length; i++) {
     if (!x[i].innerHTML.toLowerCase().includes(input)) {
